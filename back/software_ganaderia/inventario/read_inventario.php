5e90a54719e085a912b5192babe120fd5cb3a409
Samtiagovisbal16 <159793839+Samtiagovisbal16@users.noreply.github.com>
2024-11-13 22:13:03 -0500

    nuevo inventario

--- back/software_ganaderia/inventario/read_inventario.php
+++ back/software_ganaderia/inventario/read_inventario.php
@@ -1,28 +1,29 @@
 <?php
 include '../../conexion/conexion.php';
 
-$sql = "SELECT * FROM inventario";
+$sql = "SELECT inventario.id, inventario.nombre, inventario.cantidad, finca.nombre AS nombre_finca
+        FROM inventario
+        JOIN finca ON inventario.finca_id_finca = finca.id";
 $result = mysqli_query($conexion, $sql);
 
-?> <table >
+echo "<table border='1'>
 <tr>
-    <th>ID</th>
     <th>Nombre</th>
     <th>Cantidad</th>
+    <th>Finca</th>
     <th>Acciones</th>
-</tr>
-<?php
+</tr>";
+
 while ($row = mysqli_fetch_assoc($result)) {
-    ?><tr>
-        <td><?=$row['id']?></td>
-        <td><?=$row['nombre']?></td>
-        <td><?=$row['cantidad']?></td>
+    echo "<tr>
+        <td>{$row['nombre']}</td>
+        <td>{$row['cantidad']}</td>
+        <td>{$row['nombre_finca']}</td>
         <td>
-
+            <a href='update_inventario.php?id={$row['id']}'>Editar</a> |
+            <a href='delete_inventario.php?id={$row['id']}'>Eliminar</a>
         </td>
-    </tr>
-    <?php
+    </tr>";
 }
+echo "</table>";
 ?>
-</table>
-
