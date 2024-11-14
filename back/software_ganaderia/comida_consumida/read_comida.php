17cc95bb0473c5dbe58fcadb4734a18152025561
Samtiagovisbal16 <159793839+Samtiagovisbal16@users.noreply.github.com>
2024-11-13 22:24:09 -0500

    nuevo inventario

--- back/software_ganaderia/comida_consumida/read_comida.php
+++ back/software_ganaderia/comida_consumida/read_comida.php
@@ -1,31 +1,41 @@
 <?php
 include '../../conexion/conexion.php';
 
-$sql = "SELECT * FROM comida_consumida";
+$sql = "SELECT comida_consumida.id AS comida_id, comida_consumida.vacas_id_animal, comida_consumida.inventario_id, comida_consumida.fecha_consumo, comida_consumida.cantidad_consumida,
+               vacas.identificacion AS identificacion_vaca, inventario.nombre AS nombre_inventario
+        FROM comida_consumida
+        JOIN vacas ON comida_consumida.vacas_id_animal = vacas.id
+        JOIN inventario ON comida_consumida.inventario_id = inventario.id";
+
 $result = mysqli_query($conexion, $sql);
 
-?><table>
-<tr>
-    <th>ID Vaca</th>
-    <th>ID Inventario</th>
-    <th>Fecha Consumo</th>
-    <th>Cantidad Consumida</th>
-    <th>Acciones</th>
-</tr>
+if (mysqli_num_rows($result) > 0) {
+    echo "<table border='1'>
+    <tr>
+        <th>Identificación de la Vaca</th>
+        <th>Nombre del Inventario</th>
+        <th>Fecha Consumo</th>
+        <th>Cantidad Consumida</th>
+        <th>Acciones</th>
+    </tr>";
 
-<?php
-
-while ($row = mysqli_fetch_assoc($result)) {
-    ?><tr>
-        <td><?=$row['vacas_id_animal']?></td>
-        <td><?=$row['inventario_id']?></td>
-        <td><?=$row['fecha_consumo']?></td>
-        <td><?=$row['cantidad_consumida']?></td>
-        <td>
-
-        </td>
-    </tr>
-    <?php
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>
+            <td>" . $row['identificacion_vaca'] . "</td>
+            <td>" . $row['nombre_inventario'] . "</td>
+            <td>" . $row['fecha_consumo'] . "</td>
+            <td>" . $row['cantidad_consumida'] . "</td>
+            <td>
+                <a href='update_comida.php?comida_id=" . $row['comida_id'] . "'>Editar</a> |
+                <a href='delete_comida.php?comida_id=" . $row['comida_id'] . "'>Eliminar</a>
+            </td>
+        </tr>";
+    }
+    echo "</table>";
+} else {
+    echo "No hay registros disponibles.";
 }
+
+mysqli_close($conexion);
 ?>
-</table>
+
